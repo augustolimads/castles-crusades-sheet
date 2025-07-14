c34aa19fe714fedf60b003e6bab1cfa253105ff0
Augusto Lima <augusto.silva@petzpartner.com.br>
2025-07-13 22:51:41 -0300

    fix: add exception to races and classes

--- src/lib/section/RaceClasseInfo.svelte
+++ src/lib/section/RaceClasseInfo.svelte
@@ -28,19 +28,19 @@
 <div class="mt-4 flex m-auto max-w-[1200px] justify-between gap-4">
   <div id="raceInfo" class="card flex-1 py-3!">
     <h2 class="font-bold text-2xl text-left">{getRaceInfo}</h2>
-    {#if getRaceInfo === $txt('dwarf').toLowerCase().trim()}
+    {#if getRaceInfo === ($txt('dwarf').toLowerCase().trim() || 'dwarf')}
       <Dwarf />
-    {:else if getRaceInfo === $txt('elf').toLowerCase().trim()}
+    {:else if getRaceInfo === ($txt('elf').toLowerCase().trim() || 'elf')}
       <Elf />.toLowerCase().trim()
-    {:else if getRaceInfo === $txt('gnome').toLowerCase().trim()}
+    {:else if getRaceInfo === ($txt('gnome').toLowerCase().trim() || 'gnome')}
       <Gnome />
-    {:else if getRaceInfo === $txt('halfElf').toLowerCase().trim()}
+    {:else if getRaceInfo ===( $txt('halfElf').toLowerCase().trim() || 'half-elf')}
       <HalfElf />
-    {:else if getRaceInfo === $txt('halfOrc').toLowerCase().trim()}
+    {:else if getRaceInfo === ($txt('halfOrc').toLowerCase().trim() || 'half-orc')}
       <HalfOrc />
-    {:else if getRaceInfo === $txt('halfling').toLowerCase().trim()}
+    {:else if getRaceInfo === ($txt('halfling').toLowerCase().trim() || 'halfling')}
       <Halfling />
-    {:else if getRaceInfo === $txt('human').toLowerCase().trim()}
+    {:else if getRaceInfo === ($txt('human').toLowerCase().trim() || 'human')}
       <Human />
     {:else}
       <div></div>
@@ -48,29 +48,29 @@
   </div>
   <div id="classeInfo" class="card flex-1 py-3!">
     <h2 class="font-bold text-2xl text-left">{getClassInfo}</h2>
-    {#if getClassInfo === $txt('assassin').toLowerCase().trim()}
+    {#if getClassInfo === ($txt('assassin').toLowerCase().trim() || 'assassin')}
       <Assassin />
-    {:else if getClassInfo === $txt('wizard').toLowerCase().trim()}
+    {:else if getClassInfo === ($txt('wizard').toLowerCase().trim() || 'wizard')}
       <Wizard />
-    {:else if getClassInfo === $txt('rogue').toLowerCase().trim()}
+    {:else if getClassInfo === ($txt('rogue').toLowerCase().trim() || 'rogue')}
       <Rogue />
-    {:else if getClassInfo === $txt('ranger').toLowerCase().trim()}
+    {:else if getClassInfo === ($txt('ranger').toLowerCase().trim() || 'ranger')}
       <Ranger />
-    {:else if getClassInfo === $txt('paladin').toLowerCase().trim()}
+    {:else if getClassInfo === ($txt('paladin').toLowerCase().trim() || 'paladin')}
       <Paladin />
-    {:else if getClassInfo === $txt('knight').toLowerCase().trim()}
+    {:else if getClassInfo === ($txt('knight').toLowerCase().trim() || 'knight')}
       <Knight />
-    {:else if getClassInfo === $txt('illusionist').toLowerCase().trim()}
+    {:else if getClassInfo === ($txt('illusionist').toLowerCase().trim() || 'illusionist')}
       <Illusionist />
-    {:else if getClassInfo === $txt('fighter').toLowerCase().trim()}
+    {:else if getClassInfo === ($txt('fighter').toLowerCase().trim() || 'fighter')}
       <Fighter />
-    {:else if getClassInfo === $txt('druid').toLowerCase().trim()}
+    {:else if getClassInfo === ($txt('druid').toLowerCase().trim() || 'druid')}
       <Druid />
-    {:else if getClassInfo === $txt('cleric').toLowerCase().trim()}
+    {:else if getClassInfo === ($txt('cleric').toLowerCase().trim() || 'cleric')}
       <Cleric />
-    {:else if getClassInfo === $txt('bard').toLowerCase().trim()}
+    {:else if getClassInfo === ($txt('bard').toLowerCase().trim() || 'bard')}
       <Bard />
-    {:else if getClassInfo === $txt('barbarian').toLowerCase().trim()}
+    {:else if getClassInfo === ($txt('barbarian').toLowerCase().trim() || 'barbarian')}
       <Barbarian />
     {:else}
       <div></div>
