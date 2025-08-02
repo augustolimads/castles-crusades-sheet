9b43287f200f55bf42fc98fbd08759dc3cfc794b
Augusto Lima <augusto.silva@petzpartner.com.br>
2025-08-02 13:46:43 -0300

    fix: appChanges state to writable

--- src/Global/state/appChanges.ts
+++ src/Global/state/appChanges.ts
@@ -1,13 +1,15 @@
-export const appChanges = $state({
+import { get, writable } from "svelte/store";
+
+export const appChanges = writable({
     hasUnsavedChanges: false
 })
 
-export function handleInputChange(newValue?:boolean) {
-    appChanges.hasUnsavedChanges = newValue ?? true;
+export function handleInputChange(newValue?: boolean) {
+    appChanges.set({ hasUnsavedChanges: newValue ?? true })
 }
 
 export function handleBeforeUnload(event: BeforeUnloadEvent) {
-    if (appChanges.hasUnsavedChanges) {
+    if (get(appChanges).hasUnsavedChanges) {
         event.preventDefault();
         event.returnValue = '';
     }
