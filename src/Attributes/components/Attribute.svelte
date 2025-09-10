b99bba588a463ea3ae099cb1132b149f1df00791
Augusto Lima <augusto.silva@petzpartner.com.br>
2025-09-10 10:50:19 -0300

    fix: disabled image inputs

--- src/Attributes/components/Attribute.svelte
+++ src/Attributes/components/Attribute.svelte
@@ -62,7 +62,7 @@
     />
     <button
       title={desc}
-      disabled={true}
+      disabled={!canEdit}
       class={[
         'cursor-pointer badge w-10',
         { 'border-yellow-600!': score.isPrimary },
@@ -72,7 +72,7 @@
     <button
       title="Toggle Primary Attribute"
       aria-label="Toggle Primary Attribute"
-      disabled={true}
+      disabled={!canEdit}
       class={[
         'absolute -left-2.5 top-1/2 -translate-y-1/2 w-5 h-5 rounded-full flex justify-center align-center cursor-pointer bg-white',
         { 'bg-yellow-600': score.isPrimary },
--- src/Global/components/ChangeImage.svelte
+++ src/Global/components/ChangeImage.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+  import { character } from 'src/Character/state/character';
+  import { isMyCharacter } from 'src/Character/storage/characterFirebase';
   import { handleInputChange } from 'src/Global/state/appChanges';
   import { selectAllText } from 'src/Global/utils/selectAllText';
 
@@ -8,6 +10,7 @@
     onChange: (value: string) => void;
   }
   let { id, image, onChange }: Props = $props();
+  let canEdit = $derived(isMyCharacter($character));
 </script>
 
 <input
@@ -16,6 +19,7 @@
   class="px-1 cursor-pointer w-full text-sm text-white bg-zinc-900/80 border border-gray-300 rounded-md shadow-sm focus:ring-rose-500 focus:border-rose-500"
   bind:value={image}
   onfocus={selectAllText}
+  disabled={!canEdit}
   onchange={(e) => {
     handleInputChange();
     const target = e.target as HTMLInputElement;
